4e02860e4b1e11172648d3b36e7f73c4984dece9
WinnMBG <winn77.m@hotmail.com>
2022-10-28 10:43:16 +0200

    add templates + update docker

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN pip install cmake
 RUN pip install -r requirements.txt
 RUN pip install "uvicorn[all]"
 
-EXPOSE 80
+EXPOSE 8000
 
 ENTRYPOINT ["uvicorn","app:app","--host","0.0.0.0","--port","8000","--reload"]
--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI, Request;
 from redis import Redis, RedisError;
 import socket;
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 # Connect to Redis 
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def hello():
+@app.get("/", response_class=HTMLResponse)
+def hello(request: Request):
 	try:
 		visites = redis.incr("compteur")
 	except RedisError:
 		visites = "Erreur de connection Redis, compteur desactive"
 		   
-	return {"nbVisites" : visites}
+	return templates.TemplateResponse("index.html",{"request": request, "nbVisites" : visites})
 	
-
+@app.get("/{id}/", response_class=HTMLResponse)
+async def getpage(request: Request, id: int):
+	if(id%2==0):
+		return templates.TemplateResponse("index1.html",{"request": request})
+	return templates.TemplateResponse("index2.html", {"request": request})
 
